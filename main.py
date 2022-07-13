4aad3d261a6fc2b67e80b95fc89f1fe27a8d5951
whyisthesheep <77189741+whyisthesheep@users.noreply.github.com>
2022-07-13 20:27:29 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+import string
+import random
+def id_generator(size=6, chars=string.ascii_uppercase):
+  return ''.join(random.choice(chars) for _ in range(size))
+print("Wordsearch generator")
+worda = input("Word 1: ")
+wordb = input("Word 2: ")
+wordc = input("Word 3: ")
+wordd = input("Word 4: ")
+wordalen = len(worda)
+wordblen = len(wordb)
+wordclen = len(wordc)
+worddlen = len(wordd)
+avgmax = wordalen + wordblen + wordclen + worddlen / 4
+avgstart = avgmax / 2
+print(random.randint(avgstart + worda)
+maxa
+maxb
+maxc
+maxd
+while num <= 10:
+      
